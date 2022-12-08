d982620dbe668f1b588b8dba3dc6918148a97d0f
tamerly <2104arva@gmail.com>
2022-12-08 21:21:38 +0300

    basic components creating

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import { Header } from './components/Header/Header'
+import { Navbar } from './components/Navbar/Navbar'
+import { Content } from './components/Content/Content'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+            <div className='app-header'>
+                <Header />
+            </div>
+            <div className='app-body'>
+                <div className='app-body-navbar'>
+                    <Navbar />
+                </div>
+                <div className='app-body-content'>
+                    <Content />
+                </div>
+            </div>
     </div>
   );
 }
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Header = () => {
+  return (
+    <header>
+        <span>Подготовка к собеседованию</span>
+    </header>
+  )
+}
